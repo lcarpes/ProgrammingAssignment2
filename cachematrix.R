4ff7e356ea5160746b1cf6613b9be0b6c0154e6b
lcarpes <lcarpes@gmail.com>
2019-03-07 20:33:34 -0300

    inclusao dos programas referentes ao exercicio2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions are used to create a special object that store a natrix and cache´s its inverse
 
-## Write a short comment describing this function
+## This function return a list of commands to create a special matrix, in a vector
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    print("entrei no set")
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## this function calculate a inverse of matrix and returns its cache in case de repetition.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  print(data)
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
